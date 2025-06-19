f76a7b3b3242597dc3aca40fa26c0d28d764cc29
Osama Samy <149599819+Osama-Samy@users.noreply.github.com>
2025-06-19 03:44:10 +0300

    Update main.py

--- main.py
+++ main.py
@@ -185,6 +185,6 @@ def cleanup_files(*paths):
         if os.path.exists(path):
             os.remove(path)
 
-if _name_ == "_main_":
+if __name__ == "__main__":
     port = int(os.getenv("PORT", 8000))
     uvicorn.run("main:app", host="0.0.0.0", port=port, reload=False)
